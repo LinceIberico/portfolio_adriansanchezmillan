1bfefce1daa5a14cf9b9efba04d174515f5923ef
LinceIberico <61143255+LinceIberico@users.noreply.github.com>
2022-02-02 00:29:04 +0100

    aplicacion desplegada

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="Adrián Sánchez Millán">
-    <meta name="copyright" content="© 2022 Portfolio Adrián Sánchez Millán">
-    <title>Portfolio AS Millán</title>
-
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <!-- <script src="https://cdn.tailwindcss.com" defer></script> -->
-</head>
-
-<body class="bg-gray-400">
-    <nav></nav>
-    <main>
-        <div>
-            <h1 class="text-5xl text-center">Portfolio Personal</h1>
-        </div>
-    </main>
-</body>
-
-</html>
+<?php
+require_once("index.php")
+?>
